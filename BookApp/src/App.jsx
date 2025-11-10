a5a2cddd9d5f62fa1cb0303c3e3d258e628dc355
Manjul016 <manjul.23b0131187@abes.ac.in>
2025-11-10 13:02:59 +0530

    registration and login pages

---
+++ BookApp/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar"
+import Book from "./Book"
+
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    <Book />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import Link1 from "./Link1.jsx";
+function App() {
+
+  return (
+    <>
+    <Link1/>
+    </>
+  )
+}
+
+export default App
